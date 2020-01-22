96d5ff73642dd4098dce7908d6be3df7643608e6
Jack in the Boxx <kdijahsweets@gmail.com>
2020-01-22 13:21:21 -0600

    Extra Steps for Practice

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -9,43 +9,72 @@ namespace IterationStatements
         {
             //TODO
 
-            //Create a List called "numbers"
-            //Create a variable of type int with an initializer of 0
+            //done Create a List called "numbers"
+            var numbers = new List<int>();
+
+            //DONE Create a variable of type int with an initializer of 0
+            // collection of ints
+            var i = 0;
+            do
+            {// execute this code at least once
+                i++; // increment your variable by 1 i = 1
+                numbers.Add(i); // then add your variable to "numbers"
+
+            } while (i < 100); // while your variable i is less than 100
 
 
+            Console.WriteLine("Hello!");
+            // Done Create a do-while loop
+                
 
-            // Create a do-while loop
-            {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
-            }
+           
+                // DoneIncrement your variable by 1
+                // DoneThen add your variable to "numbers"
+            
             // While your variable is less than 100
-
-
-
+                       
             // Create a while loop
-            // While your variable is less than 200
+
+            while (i < 200)
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+                i++;
+                numbers.Add(i);
+            }
+                                 
+
+            // DONE While your variable is less than 200
+            {
+                // DONE Increment your variable by 1
+                //  DONE Then add your variable to "numbers"
             }
 
             Console.WriteLine("Increase:");
 
-            // Create a foreach loop
-            // Write your variable to the console
+
+            
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            // DONE Create a foreach loop
+            // DONE Write your variable to the console
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
 
-            // Create a for loop
-            // in your initializer set the value of i to 199
-            // in your conditional, as long as i is less than or equal to the length of "numbers"
-            // and as long as i is greater than or equal to 0
-            // Decrement i by 1
+            for(int a = 199; a <= numbers.Count && a >=0; a--)
             {
-                // Write to the console "numbers" at index i
+                Console.WriteLine(numbers[a]);
             }
+
+            Console.WriteLine(numbers.Count);
+            
+            //DONE  Create a for loop
+            // DONE in your initializer set the value of i to 199
+            // DONE in your conditional, as long as i is less than or equal to the length of "numbers"
+            // DONE and as long as i is greater than or equal to 0
+            // DONE Decrement i by 1
+            
         }
     }
 }
